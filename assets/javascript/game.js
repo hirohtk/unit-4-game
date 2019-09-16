909d3cf06f2532be8212d3fb2211b686546af4fe
Kensen <hirohtk@gmail.com>
2019-09-15 20:06:39 -0700

    EOD 2019_0915, fixed duplication bug. needed to only have one instance of the event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var runningTotal = 0;
 var wins = 0;
 var losses = 0;
 
-var choiceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 var choiceRed;
 var choiceYellow;
 var choiceGreen;
@@ -13,59 +13,67 @@ var choiceBlue;
 
 var newDiv;
 
-/*function unique(choice) {
-    choice = Math.floor(Math.random() * choiceArray.length);
-    var roll = choiceArray.splice(choice, 1);
-    choice = roll[0];
-    console.log(roll[0]);
-    console.log("this is running")
-}*/
+function crystalValueReset() {
+    $("#red-crystal").val(0); 
+    $("#yellow-crystal").val(0); 
+    $("#green-crystal").val(0); 
+    $("#blue-crystal").val(0);
+}
 
+function check() {
+    if (runningTotal === randomGoal) {
+        wins++;
+        $(".winLossStatus").remove();
+        newDiv = $("<div>");
+        newDiv.text("You win!");
+        newDiv.addClass("winLossStatus")
+        $(".winLossBox").prepend(newDiv);
+        $("#winNumber").text(wins);
+        crystalValueReset();
+        initialize();
+        
+    }
+    else if (runningTotal > randomGoal) {
+        losses++;
+        $(".winLossStatus").remove();
+        newDiv = $("<div>");
+        newDiv.text("You lose.");
+        newDiv.addClass("winLossStatus")
+        $(".winLossBox").prepend(newDiv);
+        $("#lossNumber").text(losses);
+        crystalValueReset();
+        initialize();
 
+    }
+    else {
+        
+    }
+}
 
 function initialize() {
-    newDiv = "";
+
+    var choiceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
     userScore = 0;
     runningTotal = 0;
     $("#yourScore").text(runningTotal);
+    var roll;
 
-    $("#red-crystal").val(0); // The val() method returns or sets the value attribute of the selected elements.
-    $("#yellow-crystal").val(0); // The val() method returns or sets the value attribute of the selected elements.
-    $("#green-crystal").val(0); // The val() method returns or sets the value attribute of the selected elements.
-    $("#blue-crystal").val(0); // The val() method returns or sets the value attribute of the selected elements.
-
-    var choiceRed = 0;
-    var choiceYellow = 0;
-    var choiceGreen = 0;
-    var choiceBlue = 0;
-    var choiceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-    choiceRed = Math.floor(Math.random() * choiceArray.length);
-    var roll = choiceArray.splice(choiceRed, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Perfect for this use.
+    var choiceRed = Math.floor(Math.random() * choiceArray.length);
+    roll = choiceArray.splice(choiceRed, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Prevents duplicate numbers on crystals
     choiceRed = roll[0];
 
-    choiceYellow = Math.floor(Math.random() * choiceArray.length);
-    var roll = choiceArray.splice(choiceYellow, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Perfect for this use.
-    choiceYellow = roll[0];
+    var choiceYellow = Math.floor(Math.random() * choiceArray.length);
+    roll = choiceArray.splice(choiceYellow, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Prevents duplicate numbers on crystals
+    choiceYellow = roll[0]; 
 
-    choiceGreen = Math.floor(Math.random() * choiceArray.length);
-    var roll = choiceArray.splice(choiceGreen, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Perfect for this use.
+    var choiceGreen = Math.floor(Math.random() * choiceArray.length);
+    roll = choiceArray.splice(choiceGreen, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Prevents duplicate numbers on crystals
     choiceGreen = roll[0];
-
-    choiceBlue = Math.floor(Math.random() * choiceArray.length);
-    var roll = choiceArray.splice(choiceBlue, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Perfect for this use.
+   
+    var choiceBlue = Math.floor(Math.random() * choiceArray.length);
+    roll = choiceArray.splice(choiceBlue, 1);   //Javascript splice() removes indexed items from an array and returns the item(s) as an array. Prevents duplicate numbers on crystals
     choiceBlue = roll[0];
-
-
-    //unique(choiceRed);
-    //unique(choiceYellow);
-    //unique(choiceGreen);
-    //unique(choiceBlue);
-
-    console.log(choiceRed);
-    console.log(choiceYellow);
-    console.log(choiceGreen);
-    console.log(choiceBlue);
 
     randomGoal = Math.floor(Math.random() * 120) + 19;
 
@@ -78,48 +86,36 @@ function initialize() {
     $("#green-crystal").val(choiceGreen); // The val() method returns or sets the value attribute of the selected elements.
     $("#blue-crystal").val(choiceBlue); // The val() method returns or sets the value attribute of the selected elements.
 
-    game();
+    console.log($("#red-crystal").val());
+    console.log($("#yellow-crystal").val());
+    console.log($("#green-crystal").val());
+    console.log($("#blue-crystal").val());
+
+    // USED TO HAVE game() CALL HERE. WHAT THIS DID WAS MAKE THE DUPLICATION ERROR.  Essentailly on("click") function ran again every game when
+    // it already existed from the first time.  So every new game had put out another "listener" on the crystals, so two identical functions were running at the same time.
 }
 
 initialize();
+game(); // critical that I put game here or else duplication error happened (see above).  
+        // if I run this once, the listener will always be there
+
 
 function game() {
 
-
     $(".crystal").on("click", function () {
 
-        console.log($(this).val());
+        console.log("Current crystal value is" + " " + $(this).val()); 
         var userScoreParser = $(this).val();
         userScore = parseInt(userScoreParser); // for some reason $(this).value wasn't an integer, so had to parseInt
 
         runningTotal += userScore;
+        console.log("running total is" + " " + runningTotal)
 
         $("#yourScore").text(runningTotal);
-        userScoreParser = 0;
+
         check();
+        
     });
-
-    function check() {
-        if (runningTotal === randomGoal) {
-            wins++;
-            newDiv = $("<div>");
-            newDiv.text("You win!");
-            $("#winNumber").prepend(newDiv);
-            $("#winNumber").text(wins);
-            initialize();
-        }
-        else if (runningTotal > randomGoal) {
-            losses++;
-            newDiv = $("<div>");
-            newDiv.text("You lose.");
-            $("#winNumber").prepend(newDiv); // still trouble here
-            $("#lossNumber").text(losses);
-            initialize();
-        }
-        else {
-
-        }
-    }
 }
 
 
